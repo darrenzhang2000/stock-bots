6d3300a09951205f556451de46bb56f1443290a7
Darren <darrenzhang200027@gmail.com>
2021-09-27 16:23:25 -0400

    added mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000
 const path = require('path')
+require('dotenv').config()
+
+
 
 // parse application/x-www-form-encoded and application/json
 const bodyParser = require('body-parser')
@@ -11,6 +14,19 @@ app.use(bodyParser.json())
 //allow cors
 var cors = require('cors')
 app.use(cors())
+
+//connect to mongoose
+const mongoose = require("mongoose")
+mongoose.connect(
+    process.env.CONNECTION_STRING,
+    { useNewUrlParser: true }
+)
+
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", () => {
+    console.log("connected to db")
+})
 
 app.get("/", (req, res) => {
     res.send("hello")
