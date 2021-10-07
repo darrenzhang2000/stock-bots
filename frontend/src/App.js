6681075ee3c49c7b1afee0158510fd3ca458fd9b
Darren <darrenzhang200027@gmail.com>
2021-10-07 18:25:02 -0400

    added signin page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css';
 import Signup from './components/signup/signup';
+import Signin from './components/signin/signin';
 import { Route } from 'react-router-dom';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       {/* <Header /> */}
       <Route exact path='/signup' component={Signup}/>
+      <Route exact path='/signin' component={Signin}/>
       {/* <Footer /> */}
     </div>
   );
