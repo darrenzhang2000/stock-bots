2fb5e81205f2dc3ebd2a7ba570050c61332bc4ea
Darren <darrenzhang200027@gmail.com>
2021-10-11 16:55:13 -0400

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Signup from './components/signup/signup';
 import Signin from './components/signin/signin';
 import { Route } from 'react-router-dom';
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
 
 function App() {
   return (
     <div>
-      {/* <Header /> */}
+      <Header />
+      <Route exact path='/' component={Signup}/>
       <Route exact path='/signup' component={Signup}/>
       <Route exact path='/signin' component={Signin}/>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
