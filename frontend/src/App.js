6b6b5b265f13ff63a1ae1dfbbcbb0d07f7417943
Darren <darrenzhang200027@gmail.com>
2021-10-24 12:37:00 -0400

    updated routes and migrated to mui5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,19 +7,23 @@ import Header from './components/header/header'
 import Footer from './components/footer/footer'
 import StockPage from './screens/stockPage/stockPage'
 import TransactionPage from './screens/transactionPage/transactionPage';
-import portfolioPage from './screens/portfolioPage/portfolioPage';
+import PortfolioPage from './screens/portfolioPage/portfolioPage';
+import AppBar from '@mui/material/AppBar';
+import Home from './screens/home/home';
+
 
 function App() {
   return (
     <div>
       <Header />
-      <Route exact path='/' component={Signup}/>
-      <Route exact path='/signup' component={Signup}/>
-      <Route exact path='/signin' component={Signin}/>
-      <Route exact path='/stockPage' component={StockPage}/>
-      <Route exact path='/transactionsPage' component={TransactionPage}/>
-      <Route exact path='/portfolioPage' component={portfolioPage}/>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/signup' component={Signup} />
+      <Route exact path='/signin' component={Signin} />
+      <Route exact path='/stockPage' component={StockPage} />
+      <Route exact path='/transactionsPage' component={TransactionPage} />
+      <Route exact path='/portfolioPage' component={PortfolioPage} />
       <Footer />
+
     </div>
   );
 }
