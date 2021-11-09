bec46bd1e98122b51004c9c8fc7391a7011b947b
Darren <darrenzhang200027@gmail.com>
2021-11-09 15:33:42 -0500

    trading algo checkpoint

--- trading-microservice/app.py
+++ trading-microservice/app.py
@@ -2,8 +2,11 @@ import pymongo
 from tradingAlgo import stockActions
 from flask import Flask, request, jsonify
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 client = pymongo.MongoClient('mongodb+srv://testuser:testuser123@cluster0.9fxli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 db = client['myFirstDatabase']
@@ -23,4 +26,4 @@ def getStockActions():
 tickers = ['GOOGL', 'AAPL', 'AMZN', 'NFLX', 'FB']
 
 if __name__ == '__main__':
-    app.run(port=6000)
+    app.run(port=8000)
