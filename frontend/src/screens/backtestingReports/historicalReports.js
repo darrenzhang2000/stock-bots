5e859658320ac833f855d9531b17a3fffee34dc6
Darren <darrenzhang200027@gmail.com>
2021-12-11 17:30:16 -0500

    rounded numbers to 2 decimal places

--- frontend/src/screens/backtestingReports/historicalReports.js
+++ frontend/src/screens/backtestingReports/historicalReports.js
@@ -1,3 +1,6 @@
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -6,57 +9,54 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
-import React, { useEffect, useState } from 'react';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import React, { useState } from 'react';
 import AAPL_Json from './jsonTestReports/AAPL_test_report.json';
-import ADBE_Json from './jsonTestReports/ADBE_test_report.json'
-import AMZN_Json from './jsonTestReports/AMZN_test_report.json'
-import AVGO_Json from './jsonTestReports/AVGO_test_report.json'
-import BAC_Json from './jsonTestReports/BAC_test_report.json'
-import CRM_Json from './jsonTestReports/CRM_test_report.json'
-import CSCO_Json from './jsonTestReports/CSCO_test_report.json'
-import DIS_Json from './jsonTestReports/DIS_test_report.json'
-import FB_Json from './jsonTestReports/FB_test_report.json'
-import GOOGL_Json from './jsonTestReports/GOOGL_test_report.json'
-import JNJ_Json from './jsonTestReports/JNJ_test_report.json'
-import JPM_Json from './jsonTestReports/JPM_test_report.json'
-import MSFT_Json from './jsonTestReports/MSFT_test_report.json'
-import NFLX_Json from './jsonTestReports/NFLX_test_report.json'
-import PFE_Json from './jsonTestReports/PFE_test_report.json'
-import PG_Json from './jsonTestReports/PG_test_report.json'
-import TMO_Json from './jsonTestReports/TMO_test_report.json'
-import TSLA_Json from './jsonTestReports/TSLA_test_report.json'
+import ADBE_Json from './jsonTestReports/ADBE_test_report.json';
+import AMZN_Json from './jsonTestReports/AMZN_test_report.json';
+import AVGO_Json from './jsonTestReports/AVGO_test_report.json';
+import BAC_Json from './jsonTestReports/BAC_test_report.json';
+import CRM_Json from './jsonTestReports/CRM_test_report.json';
+import CSCO_Json from './jsonTestReports/CSCO_test_report.json';
+import DIS_Json from './jsonTestReports/DIS_test_report.json';
+import FB_Json from './jsonTestReports/FB_test_report.json';
+import GOOGL_Json from './jsonTestReports/GOOGL_test_report.json';
+import JNJ_Json from './jsonTestReports/JNJ_test_report.json';
+import JPM_Json from './jsonTestReports/JPM_test_report.json';
+import MSFT_Json from './jsonTestReports/MSFT_test_report.json';
+import NFLX_Json from './jsonTestReports/NFLX_test_report.json';
+import PFE_Json from './jsonTestReports/PFE_test_report.json';
+import PG_Json from './jsonTestReports/PG_test_report.json';
+import TMO_Json from './jsonTestReports/TMO_test_report.json';
+import TSLA_Json from './jsonTestReports/TSLA_test_report.json';
 
 const stocks = ['AAPL', 'ADBE', 'AMZN', 'AVGO', 'BAC', 'CRM', 'CSCO', 'DIS', 'FB', 'GOOGL', 'JNJ', 'JPM', 'MSFT', 'NFLX', 'PFE', 'PG', 'TMO', 'TSLA']
 const reports = ['AAPL_test_report.json', 'ADBE_test_report.json', 'AMZN_test_report.json', 'AVGO_test_report.json', 'BAC_test_report.json', 'CRM_test_report.json', 'CSCO_test_report.json', 'DIS_test_report.json', 'FB_test_report.json', 'GOOGL_test_report.json', 'JNJ_test_report.json', 'JPM_test_report.json', 'MSFT_test_report.json', 'NFLX_test_report.json', 'PFE_test_report.json', 'PG_test_report.json', 'TMO_test_report.json', 'TSLA_test_report.json']
 const stockMap = {
-    'AAPL': AAPL_Json, 
+    'AAPL': AAPL_Json,
     'ADBE': ADBE_Json,
-    'AMZN': AMZN_Json, 
-    'AVGO': AVGO_Json, 
-    'BAC': BAC_Json, 
-    'CRM': CRM_Json, 
-    'CSCO': CSCO_Json, 
+    'AMZN': AMZN_Json,
+    'AVGO': AVGO_Json,
+    'BAC': BAC_Json,
+    'CRM': CRM_Json,
+    'CSCO': CSCO_Json,
     'DIS': DIS_Json,
-    'FB': FB_Json, 
-    'GOOGL': GOOGL_Json, 
-    'JNJ': JNJ_Json, 
-    'JPM': JPM_Json, 
-    'MSFT': MSFT_Json, 
-    'NFLX': NFLX_Json, 
-    'PFE': PFE_Json, 
-    'PG': PG_Json, 
-    'TMO': TMO_Json, 
+    'FB': FB_Json,
+    'GOOGL': GOOGL_Json,
+    'JNJ': JNJ_Json,
+    'JPM': JPM_Json,
+    'MSFT': MSFT_Json,
+    'NFLX': NFLX_Json,
+    'PFE': PFE_Json,
+    'PG': PG_Json,
+    'TMO': TMO_Json,
     'TSLA': TSLA_Json
 }
 
 
 const HistoricalReports = () => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState(AAPL_Json)
     const [anchorEl, setAnchorEl] = React.useState(null);
-    const [currentReport, setCurrentReport] = React.useState("")
+    const [currentReport, setCurrentReport] = React.useState("AAPL")
     const open = Boolean(anchorEl);
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
@@ -73,7 +73,12 @@ const HistoricalReports = () => {
 
     return (
         <div>
-            HistoricalReports page
+            <Typography variant="h6" className="font-link" sx={{ marginBottom: '32px', marginTop: '32px' }}> Backtesting Reports</Typography>
+            <Typography variant="h6" className="font-link" sx={{ marginBottom: '32px', marginTop: '32px' }}> Displaying backtesting report for: <Typography variant="h6" sx={{ fontWeight: 'bold', display: 'inline' }}> {currentReport}</Typography> </Typography>
+            <Typography variant="h6" className="font-link" sx={{ marginBottom: '32px', marginTop: '32px' }}>
+                Summary: Over a three year period, this trading algorithm made a net gain of ${parseFloat(data[data.length - 1].Total - 10000).toFixed(2)}.
+                This is a {parseFloat(100 * (data[data.length - 1].Total - 10000) / 10000).toFixed(2)}% increase. Initially starting out with $10000 on 2018-12-06, running this algorithm once every business
+                day for the last 3 years ended with ${parseFloat(data[data.length - 1].Total).toFixed(2)} on 2021-12-08. </Typography>
             <Button
                 id="basic-button"
                 aria-controls="basic-menu"
@@ -81,9 +86,9 @@ const HistoricalReports = () => {
                 aria-expanded={open ? 'true' : undefined}
                 onClick={handleClick}
             >
-                Choose your test report
+                Choose a different test report
             </Button>
-            Displaying backtesting report for: {currentReport}
+
             <Menu
                 id="basic-menu"
                 anchorEl={anchorEl}
@@ -95,9 +100,9 @@ const HistoricalReports = () => {
             >
                 {
                     stocks.map((stock, idx) => <MenuItem
-                     id={stock} 
-                     onClick={handleOpenReport}
-                     > {stock} </MenuItem>)
+                        id={stock}
+                        onClick={handleOpenReport}
+                    > {stock} </MenuItem>)
                 }
             </Menu>
 
@@ -124,10 +129,10 @@ const HistoricalReports = () => {
                                     <TableCell component="th" scope="row">
                                         {row.Date}
                                     </TableCell>
-                                    <TableCell align="right">{row.Price}</TableCell>
+                                    <TableCell align="right">{parseFloat(row.Price).toFixed(2)}</TableCell>
                                     <TableCell align="right">{row.Quantity}</TableCell>
-                                    <TableCell align="right">{row["Spending Power"]}</TableCell>
-                                    <TableCell align="right">{row.Total}</TableCell>
+                                    <TableCell align="right">{parseFloat(row["Spending Power"]).toFixed(2)}</TableCell>
+                                    <TableCell align="right">{parseFloat(row.Total).toFixed(2)}</TableCell>
                                     <TableCell align="right">{row.Report}</TableCell>
                                 </TableRow>
                             ))}
